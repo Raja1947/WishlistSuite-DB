bb20c5dc6801a6243917cc8260a4a3a7d23ed243
Raja Dhoundiyal <rajadhoundiyalpda5@gmail.com>
2026-06-24 16:54:36 +0530

    icon fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "WishlistSuite DB",
+  title: "WishlistSuite Data",
   description: "WishlistSuite Database Dashboard",
 };
 
